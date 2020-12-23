7d2de4a4346c24d6e078f09d5d2b16bd37a6fc8f
Ohtani <v24kuong@gmail.com>
2020-12-23 20:05:02 +0900

    form

--- Test.php
+++ Test.php
@@ -1,10 +1,29 @@
 <?php
+  echo htmlspecialchars ($_POST['name']);
+?> さんの今日の運勢は
+
+<?php
   $num = rand(0, 2);
   if ($num == 0) {
-    echo "今日の運勢は大吉です。";
+    echo "大吉です。";
   } else if ($num == 1) {
-    echo "今日の運勢は吉です。";
+    echo "吉です。";
   } else {
-    echo "今日の運勢は凶です。";
+    echo "凶です。";
   }
+?>
+
+<p>今日の運勢を投稿してみよう!!</p>
+<form method="post" action="Test.php">
+<input type="text" name="toukou" size="60">
+<input type="submit" value="送信">
+</form>
+<hr>
+<?php
+$ichiran = file_get_contents('Test.txt');
+if (@$_POST['toukou']) {
+  $ichiran = htmlspecialchars($_POST['toukou']) . "<hr>$ichiran";
+  file_put_contents('Test.txt', $ichiran);
+}
+echo $ichiran;
 ?>
